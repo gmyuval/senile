38a347941241b0d3b09bdfe0c5cb54ed1da3d6c4
yuvalm <yuvalm@iguazio.com>
2018-01-03 18:59:11 +0200

    Created SenileBot class

--- senile.py
+++ senile.py
@@ -2,18 +2,9 @@ import os
 import time
 import re
 import pdb
+import boto3
 from slackclient import SlackClient
 
-# instantiate Slack client
-slack_client = SlackClient('')
-# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-# starterbot's user ID in Slack: value is assigned after the bot starts up
-starterbot_id = None
-
-# constants
-RTM_READ_DELAY = 1  # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@(|[WU].+)>(.*)"
 ACTION_MSG1 = {
     "text": "Eh, forgot to clock-in today?",
     "attachments": [
@@ -53,63 +44,92 @@ ACTION_MSG1 = {
 }
 
 
-def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == starterbot_id:
-                return message, event["channel"]
-    return None, None
+class SenileBot(object):
+    RTM_READ_DELAY = 1  # 1 second delay between reading from RTM
+    MENTION_REGEX = "^<@(|[WU].+)>(.*)"
 
+    def __init__(self):
+        self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+        self.bot_id = None
+        self.available_commands = {
+            'register': self.register_user,
+        }
+        self.connect()
+        self.dyndb = boto3.client('dynamodb', endpoint_url='http://{}:{}'.format('192.168.206.41', '8081'),
+                                  iguazio_management_url='http://{}:{}'.format('192.168.202.16', '8001'),
+                                  is_iguazio_api=True, iguazio_management_username='iguazio',
+                                  iguazio_management_password='Password1', region_name='lala')
+        if not self.bot_id:
+            raise RuntimeError('Failed connecting to slack.')
 
-def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+    def connect(self):
+        if self.slack_client.rtm_connect():
+            print("Senile Bot connected and running!")
+            # Read bot's user ID by calling Web API method `auth.test`
+            self.bot_id = self.slack_client.api_call("auth.test")["user_id"]
+        else:
+            print("Connection failed. Exception traceback printed above.")
 
+    def run_loop(self):
+        while True:
+            events = self.slack_client.rtm_read()
+            for event in events:
+                print(event)
+            command, channel, user = self.parse_bot_commands(events)
+            if command:
+                self.handle_command(command, channel, user)
+            time.sleep(self.RTM_READ_DELAY)
 
-def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+    def parse_bot_commands(self, slack_events):
+        """
+            Parses a list of events coming from the Slack RTM API to find bot commands.
+            If a bot command is found, this function returns a tuple of command and channel.
+            If its not found, then this function returns None, None.
+        """
+        for event in slack_events:
+            if event["type"] == "message" and not "subtype" in event:
+                direct_mention = re.search(self.MENTION_REGEX, event['text'])
+                if direct_mention and direct_mention.group(1) == self.bot_id:
+                    return direct_mention.group(2).strip(), event['channel'], event['user']
+                elif self.slack_client.api_call('conversations.info', channel=event['channel'])['channel']['is_im']:
+                    return event['text'], event['channel'], event['user']
+        return None, None, None
 
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+    def handle_command(self, command_text, channel, user_id):
+        """
+            Executes bot command if the command is known
+        """
+        # Default response is help text for the user
+        default_response = "Not sure what you mean. Try one of the existing commands:\n{}"\
+            .format('\n'.join(self.available_commands.iterkeys()))
 
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
+        # Finds and executes the given command, filling in response
+        response = None
+        # This is where you start to implement more commands!
+        command = command_text.split()[0]
+        if command.lower() in self.available_commands:
+            response = self.available_commands[command.lower()](user_id, ' '.join(command_text.split()[1:]))
+
+        # Sends the response back to the channel
+        self.slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=response or default_response
+        )
+
+    def register_user(self, user_id, command_text):
+        command_syntax = "Syntax: register <synel_user> <synel_password>"
+        match = re.search(r'(\d+)\s+(\S+)$', command_text)
+        if not match:
+            return 'Wrong syntax. {}'.format(command_syntax)
+        self.dyndb.put_item(TableName='registered_users', Bucket='bigdata',
+                            Item=dict(slack_user=dict(S=user_id),
+                                      synel_user=dict(S=match.group(1)),
+                                      synel_pass=dict(S=match.group(2))))
+        return 'Registered user {} with password {}'.format(match.group(1), match.group(2))
+
 
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect():
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-        while True:
-            events = slack_client.rtm_read()
-            for event in events:
-                print event
-            command, channel = parse_bot_commands(events)
-            if command:
-                handle_command(command, channel)
-            time.sleep(RTM_READ_DELAY)
-    else:
-        print("Connection failed. Exception traceback printed above.")
+    sc = SenileBot()
+    sc.run_loop()
